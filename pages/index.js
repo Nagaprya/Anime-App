dad9628c5804e6dc8f05622f4a19625c00c9d080
joy-1998 <56531360+joy-1998@users.noreply.github.com>
2024-03-03 09:33:46 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -38,6 +38,8 @@ const modalStyle = {
   p: 4,
 };
 
+Adding this line for Build Failure
+
 export default function Home() {
   const [animeGenre, setAnimeGenre] = useState("");
   const [response, setResponse] = useState(null);
